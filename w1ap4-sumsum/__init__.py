6e259b10b43a2dedc344fed7be70338f1fc65c7b
okskola <53981730+okskola@users.noreply.github.com>
2023-09-13 14:32:42 +0300

    sumsum

--- w1ap4-sumsum/__init__.py
+++ w1ap4-sumsum/__init__.py
@@ -16,3 +16,18 @@ def sumsum10():
     """checks that sumsum(4,1) is 10"""
     check50.run("./w1ap4-sumsum").stdin("4").stdin("1").stdout("Sumsum: 10").exit()
 
+@check50.check(compiles)
+def sumsum21():
+    """checks that sumsum(3,2) is 21"""
+    check50.run("./w1ap4-sumsum").stdin("3").stdin("2").stdout("Sumsum: 21").exit()
+
+
+@check50.check(compiles)
+def sumsum1540():
+    """checks that sumsum(4,3) is 1540"""
+    check50.run("./w1ap4-sumsum").stdin("4").stdin("3").stdout("Sumsum: 1540").exit()
+
+@check50.check(compiles)
+def test0():
+    """rejects a height of 0"""
+    check50.run("./mario").stdin("0").reject()

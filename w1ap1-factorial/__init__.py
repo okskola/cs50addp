c2e51ac2b6f11ece1cb26f5f019ae6400aadfdd3
okskola <53981730+okskola@users.noreply.github.com>
2023-09-13 13:18:47 +0300

    Update __init__.py

--- w1ap1-factorial/__init__.py
+++ w1ap1-factorial/__init__.py
@@ -12,6 +12,21 @@ def compiles():
     check50.c.compile("w1ap1-factorial.c", lcs50=True)
 
 @check50.check(compiles)
-def emma():
-    """responds to name Emma"""
-    check50.run("./w1ap1-factorial").stdin("5").stdout("120").exit()
+def f5():
+    """checks that 5! equals 120"""
+    check50.run("./w1ap1-factorial").stdin("5").stdout("125").exit()
+
+@check50.check(compiles)
+def f10():
+    """checks that 10! equals 3628800"""
+    check50.run("./w1ap1-factorial").stdin("10").stdout("3628800").exit()
+
+@check50.check(compiles)
+def f1():
+    """checks that 1! equals 1"""
+    check50.run("./w1ap1-factorial").stdin("1").stdout("1").exit()
+
+@check50.check(compiles)
+def f0():
+    """checks that 0! equals 1"""
+    check50.run("./w1ap1-factorial").stdin("0").stdout("1").exit()

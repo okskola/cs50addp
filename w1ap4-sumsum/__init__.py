3e7022d8f552ab14119afc20815122cde5671e47
okskola <53981730+okskola@users.noreply.github.com>
2023-09-13 14:34:55 +0300

    sumsum

--- w1ap4-sumsum/__init__.py
+++ w1ap4-sumsum/__init__.py
@@ -12,6 +12,16 @@ def compiles():
     check50.c.compile("w1ap4-sumsum.c", lcs50=True)
 
 @check50.check(compiles)
+def test0():
+    """rejects n<1"""
+    check50.run("./w1ap4-sumsum").stdin("0").reject()
+
+@check50.check(compiles)
+def test0():
+    """rejects t<1"""
+    check50.run("./w1ap4-sumsum").stdin("1").stdin("0").reject()
+
+@check50.check(compiles)
 def sumsum10():
     """checks that sumsum(4,1) is 10"""
     check50.run("./w1ap4-sumsum").stdin("4").stdin("1").stdout("Sumsum: 10").exit()
@@ -27,7 +37,3 @@ def sumsum1540():
     """checks that sumsum(4,3) is 1540"""
     check50.run("./w1ap4-sumsum").stdin("4").stdin("3").stdout("Sumsum: 1540").exit()
 
-@check50.check(compiles)
-def test0():
-    """rejects a height of 0"""
-    check50.run("./mario").stdin("0").reject()

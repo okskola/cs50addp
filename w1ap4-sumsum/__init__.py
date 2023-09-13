abc66d31499201f26b041661810d04ef01b2393a
okskola <53981730+okskola@users.noreply.github.com>
2023-09-13 19:12:53 +0300

    .

--- w1ap4-sumsum/__init__.py
+++ w1ap4-sumsum/__init__.py
@@ -16,23 +16,23 @@ def testn0():
     """rejects n<1"""
     check50.run("./w1ap4-sumsum").stdin("0").stdin("1").reject()
 
-@check50.check(compiles)
+@check50.check(compiles, testn0)
 def testt0():
     """rejects t<1"""
-    check50.run("./w1ap4-sumsum").stdin("1").stdin("0").stdin("1").reject()
+    check50.run("./w1ap4-sumsum").stdin("1").stdin("0").reject()
 
-@check50.check(compiles)
+@check50.check(compiles, testn0, testt0)
 def sumsum10():
     """checks that sumsum(4,1) is 10"""
     check50.run("./w1ap4-sumsum").stdin("4").stdin("1").stdout("Sumsum: 10").exit()
 
-@check50.check(compiles)
+@check50.check(compiles, testn0, testt0)
 def sumsum21():
     """checks that sumsum(3,2) is 21"""
     check50.run("./w1ap4-sumsum").stdin("3").stdin("2").stdout("Sumsum: 21").exit()
 
 
-@check50.check(compiles)
+@check50.check(compiles, testn0, testt0)
 def sumsum1540():
     """checks that sumsum(4,3) is 1540"""
     check50.run("./w1ap4-sumsum").stdin("4").stdin("3").stdout("Sumsum: 1540").exit()

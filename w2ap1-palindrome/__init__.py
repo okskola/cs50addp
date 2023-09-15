83f5772d494f67738affe758766df3c99d9f56a3
okskola <53981730+okskola@users.noreply.github.com>
2023-09-15 22:49:05 +0300

    .

--- w2ap1-palindrome/__init__.py
+++ w2ap1-palindrome/__init__.py
@@ -24,39 +24,39 @@ def toomanyargs():
 @check50.check(compiles)
 def evenpalindrome():
     """check if even characters palindrome gives output of yes"""
-    check50.run("./w1ap4-sumsum abccba").stdout("Yes").exit()
+    check50.run("./w2ap1-palindrome abccba").stdout("Yes").exit()
 
 @check50.check(compiles)
 def oddpalindrome():
     """check if odd characters palindrome gives output of yes"""
-    check50.run("./w1ap4-sumsum abcdcba").stdout("Yes").exit()
+    check50.run("./w2ap1-palindrome abcdcba").stdout("Yes").exit()
 
 @check50.check(compiles)
 def shortpalindrome():
     """check if short palindrome gives output of yes"""
-    check50.run("./w1ap4-sumsum k").stdout("Yes").exit()
+    check50.run("./w2ap1-palindrome k").stdout("Yes").exit()
 
 @check50.check(compiles)
 def short2palindrome():
     """check if short palindrome gives output of yes"""
-    check50.run("./w1ap4-sumsum kk").stdout("Yes").exit()
+    check50.run("./w2ap1-palindrome kk").stdout("Yes").exit()
 
 @check50.check(compiles)
 def short2palindrome():
     """check that program is case-insensitive"""
-    check50.run("./w1ap4-sumsum AbCcBa").stdout("Yes").exit()
+    check50.run("./w2ap1-palindrome AbCcBa").stdout("Yes").exit()
 
 @check50.check(compiles)
 def palindromewithspace():
     """check if palindrome with space gives output of yes"""
-    check50.run("./w1ap4-sumsum \"Yawn a more Roman way\"").stdout("Yes").exit()
+    check50.run("./w2ap1-palindrome \"Yawn a more Roman way\"").stdout("Yes").exit()
 
 @check50.check(compiles)
 def nopalindrome():
     """check that program outputs No for non-palondrome"""
-    check50.run("./w1ap4-sumsum abcdba").stdout("No").exit()
+    check50.run("./w2ap1-palindrome abcdba").stdout("No").exit()
 
 @check50.check(compiles)
 def nopalindrome2():
     """check that program outputs No for numeric non-palondrome"""
-    check50.run("./w1ap4-sumsum 12").stdout("No").exit()
+    check50.run("./w2ap1-palindrome 12").stdout("No").exit()

69f07bd3906aab1db7423c92089f1c24089b96b2
okskola <53981730+okskola@users.noreply.github.com>
2023-09-15 22:46:59 +0300

    palindromes

---
+++ w2ap1-palindrome/__init__.py
@@ -0,0 +1,62 @@
+import check50
+import check50.c
+
+@check50.check()
+def exists():
+    """w2ap1-palindrome.c exists"""
+    check50.exists("w2ap1-palindrome.c")
+
+@check50.check(exists)
+def compiles():
+    """w2ap1-palindrome.c compiles"""
+    check50.c.compile("w2ap1-palindrome.c", lcs50=True)
+
+@check50.check(compiles)
+def noargs():
+    """check number of command line arguments"""
+    check50.run("./w2ap1-palindrome").exit(1)
+
+@check50.check(compiles)
+def toomanyargs():
+    """check number of command line arguments"""
+    check50.run("./w2ap1-palindrome aaa bbb").exit(1)
+
+@check50.check(compiles)
+def evenpalindrome():
+    """check if even characters palindrome gives output of yes"""
+    check50.run("./w1ap4-sumsum abccba").stdout("Yes").exit()
+
+@check50.check(compiles)
+def oddpalindrome():
+    """check if odd characters palindrome gives output of yes"""
+    check50.run("./w1ap4-sumsum abcdcba").stdout("Yes").exit()
+
+@check50.check(compiles)
+def shortpalindrome():
+    """check if short palindrome gives output of yes"""
+    check50.run("./w1ap4-sumsum k").stdout("Yes").exit()
+
+@check50.check(compiles)
+def short2palindrome():
+    """check if short palindrome gives output of yes"""
+    check50.run("./w1ap4-sumsum kk").stdout("Yes").exit()
+
+@check50.check(compiles)
+def short2palindrome():
+    """check that program is case-insensitive"""
+    check50.run("./w1ap4-sumsum AbCcBa").stdout("Yes").exit()
+
+@check50.check(compiles)
+def palindromewithspace():
+    """check if palindrome with space gives output of yes"""
+    check50.run("./w1ap4-sumsum \"Yawn a more Roman way\"").stdout("Yes").exit()
+
+@check50.check(compiles)
+def nopalindrome():
+    """check that program outputs No for non-palondrome"""
+    check50.run("./w1ap4-sumsum abcdba").stdout("No").exit()
+
+@check50.check(compiles)
+def nopalindrome2():
+    """check that program outputs No for numeric non-palondrome"""
+    check50.run("./w1ap4-sumsum 12").stdout("No").exit()

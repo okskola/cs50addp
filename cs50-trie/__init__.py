b10ee5f0c16f64890190f42f754eb12c22b0b132
okskola <53981730+okskola@users.noreply.github.com>
2023-10-07 19:50:48 +0000

    trie

--- cs50-trie/__init__.py
+++ cs50-trie/__init__.py
@@ -21,38 +21,33 @@ def toomanyargs():
     """check number of command line arguments (too many args)"""
     check50.run("./trie aaa.txt bbb.txt").exit(1)
 
-# @check50.check(compiles)
-# def test1():
-#     """checks for no argument"""
-#     check50.run("./trie").stdout("abc\nbcd").exit()
+@check50.check(compiles)
+ def test1():
+     """checks for Molly"""
+     check50.run("./trie dog_names.txt").stdin("Molly").stdout("Found!").exit()
 
-# @check50.check(compiles)
-# def test2():
-#     """checks for lowercase without spaces, unsorted"""
-#     check50.run("./trie").stdin("bcd").stdin("abc").stdout("abc\nbcd").exit()
+@check50.check(compiles)
+ def test2():
+     """checks for Lucy"""
+     check50.run("./trie dog_names.txt").stdin("Lucy").stdout("Found!").exit()
 
-# @check50.check(compiles)
-# def test3():
-#     """checks for equal"""
-#     check50.run("./trie").stdin("57931").stdin("57931").stdout("57931\n57931").exit()
+@check50.check(compiles)
+ def test3():
+     """checks for Prudence"""
+     check50.run("./trie dog_names.txt").stdin("Prudence").stdout("Not Found.").exit()
 
-# @check50.check(compiles)
-# def test4():
-#     """checks for lowercase with spaces, equal"""
-#     check50.run("./trie").stdin("the test").stdin("t h et e s t").stdout("the test\nt h et e s t").exit()
+@check50.check(compiles)
+ def test4():
+     """checks for mOLLy (ignores case)"""
+     check50.run("./trie dog_names.txt").stdin("mOLLy").stdout("Found!").exit()
 
-# @check50.check(compiles)
-# def test5():
-#     """checks for different case, without spaces, sorted"""
-#     check50.run("./trie").stdin("aBc").stdin("BcD").stdout("aBc\nBcD").exit()
+@check50.check(compiles)
+ def test5():
+     """checks for other names (Found)"""
+     check50.run("./trie other_names.txt").stdin("JENNIFER").stdout("Found!").exit()
 
-# @check50.check(compiles)
-# def test6():
-#     """checks for different case, without spaces, unsorted"""
-#     check50.run("./trie").stdin("aBcd").stdin("AbAd").stdout("AbAd\naBcd").exit()
-
-# @check50.check(compiles)
-# def test7():
-#     """checks for different case, with spaces, unsorted"""
-#     check50.run("./trie").stdin("a Bc d").stdin("aA            cd").stdout("aA            cd\na Bc d").exit()
+@check50.check(compiles)
+ def test6():
+     """checks for other names (Not Found)"""
+     check50.run("./trie other_names.txt").stdin("Jenni").stdout("Not Found.").exit()
 

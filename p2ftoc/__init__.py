090e7d2d7c5adee7b8fcdff41b9aa3001f72fb1d
okskola <53981730+okskola@users.noreply.github.com>
2024-09-15 19:08:43 +0000

    p2ftoc

--- p2ftoc/__init__.py
+++ p2ftoc/__init__.py
@@ -10,3 +10,17 @@ def test1():
     """checks integer """
     check50.run("python3 p2ftoc.py").stdin("-40").stdin("32").stdin("68").stdin("98").stdin("212").stdout("212.0ºF = 100.0ºC").exit()
 
+@check50.check(exists)
+def test2():
+    """checks floats """
+    check50.run("python3 p2ftoc.py").stdin("-40").stdin("32").stdin("68").stdin("98.6").stdin("212").stdout("98.6ºF = 37.0ºC").exit()
+
+@check50.check(exists)
+def test3():
+    """checks inpuit rounding """
+    check50.run("python3 p2ftoc.py").stdin("-40").stdin("32").stdin("68").stdin("98.6").stdin("212.00005").stdout("212.0ºF = 100.0ºC").exit()
+
+@check50.check(exists)
+def test3():
+    """checks output rounding """
+    check50.run("python3 p2ftoc.py").stdin("-40").stdin("32").stdin("68").stdin("98.6").stdin("2").stdout("2.0ºF = -16.7ºC").exit()

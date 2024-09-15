88cdb3d3a02daad70ccdabd02eacba47b573e8ae
okskola <53981730+okskola@users.noreply.github.com>
2024-09-15 19:16:25 +0000

    testing brach

--- p2total_notdone/p2total.py
+++
@@ -1,9 +0,0 @@
-def total( items, prices ):
-    return sum(prices)
-
-
-items = ["Apple", "Banana", "Carrot"]
-prices = [3,5,7]
-for i in range( len(items) ):
-    print( items[i], prices[i] )
-print( "Total =", total(items, prices) )

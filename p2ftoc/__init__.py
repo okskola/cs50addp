85c722ea2b9341416a74361ffb0cd30d72540810
okskola <53981730+okskola@users.noreply.github.com>
2024-09-15 19:03:47 +0000

    p2ftoc

---
+++ p2ftoc/__init__.py
@@ -0,0 +1,12 @@
+import check50
+
+@check50.check()
+def exists():
+    """p2ftoc.py exists"""
+    check50.exists("p2ftoc.py")
+
+@check50.check(exists)
+def test1():
+    """checks integer """
+    check50.run("python3 p2digitnum.py").stdin("-40").stdin("32").stdin("68").stdin("98").stdin("212").stdout("212.0ºF = 100.0ºC").exit()
+

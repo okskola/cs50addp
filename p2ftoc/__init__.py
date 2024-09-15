cc22b66fd670c11aa1db0523b430fd11b006194b
okskola <53981730+okskola@users.noreply.github.com>
2024-09-15 19:09:24 +0000

    p2ftoc

--- p2ftoc/__init__.py
+++ p2ftoc/__init__.py
@@ -21,6 +21,6 @@ def test3():
     check50.run("python3 p2ftoc.py").stdin("-40").stdin("32").stdin("68").stdin("98.6").stdin("212.00005").stdout("212.0ºF = 100.0ºC").exit()
 
 @check50.check(exists)
-def test3():
+def test4():
     """checks output rounding """
     check50.run("python3 p2ftoc.py").stdin("-40").stdin("32").stdin("68").stdin("98.6").stdin("2").stdout("2.0ºF = -16.7ºC").exit()

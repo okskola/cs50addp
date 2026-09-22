5289305cb514c99d31474c44e2ac9d9aeaf3073a
okskola <53981730+okskola@users.noreply.github.com>
2026-09-22 18:22:45 +0000

    p1blivums

---
+++ p1blivums/__init__.py
@@ -0,0 +1,36 @@
+import check50
+
+@check50.check()
+def exists():
+    """p1blivums.py eksistē"""
+    check50.exists("p1blivums.py")
+
+
+@check50.check(exists)
+def test_invalid():
+    """checks -100"""
+    check50.run("python3 p1blivums.py").stdin("-100").stdout("Nederīgs").exit()
+
+
+@check50.check(exists)
+def test_low():
+    """checks 1999"""
+    check50.run("python3 p1blivums.py").stdin("1999").stdout("Zems").exit()
+
+
+@check50.check(exists)
+def test_lower_boundary():
+    """checks 2000"""
+    check50.run("python3 p1blivums.py").stdin("2000").stdout("Vidējs").exit()
+
+
+@check50.check(exists)
+def test_upper_boundary():
+    """checks 6000"""
+    check50.run("python3 p1blivums.py").stdin("6000").stdout("Vidējs").exit()
+
+
+@check50.check(exists)
+def test_high():
+    """checks 6001"""
+    check50.run("python3 p1blivums.py").stdin("6001").stdout("Augsts").exit()

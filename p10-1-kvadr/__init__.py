0c1185285a59b5d53ff6a565814499edd91b5352
okskola <53981730+okskola@users.noreply.github.com>
2026-09-25 13:34:54 +0300

    p10-1-kvadr

--- p10-1-kvadr/__init__.py
+++ p10-1-kvadr/__init__.py
@@ -3,8 +3,8 @@ import check50
 
 @check50.check()
 def exists():
-    """Pārbauda, vai quadratic.py eksistē"""
-    check50.exists("quadratic.py")
+    """Pārbauda, vai p10-1-kvadr.py eksistē"""
+    check50.exists("p10-1-kvadr.py")
 
 
 # Divas dažādas reālas saknes
@@ -12,7 +12,7 @@ def exists():
 @check50.check(exists)
 def two_roots():
     """Pārbauda vienādojumu ar divām dažādām reālām saknēm"""
-    check50.run("python3 quadratic.py").stdin("1").stdin("-5").stdin("6").stdout("x1 = 3.0").stdout("x2 = 2.0").exit()
+    check50.run("python3 p10-1-kvadr.py").stdin("1").stdin("-5").stdin("6").stdout("x1 = 3.0").stdout("x2 = 2.0").exit()
 
 
 # Divas vienādas reālas saknes
@@ -20,7 +20,7 @@ def two_roots():
 @check50.check(exists)
 def equal_roots():
     """Pārbauda vienādojumu ar divām vienādām saknēm"""
-    check50.run("python3 quadratic.py").stdin("1").stdin("-4").stdin("4").stdout("x1 = 2.0").stdout("x2 = 2.0").exit()
+    check50.run("python3 p10-1-kvadr.py").stdin("1").stdin("-4").stdin("4").stdout("x1 = 2.0").stdout("x2 = 2.0").exit()
 
 
 # Nav reālu sakņu
@@ -28,7 +28,7 @@ def equal_roots():
 @check50.check(exists)
 def no_roots():
     """Pārbauda vienādojumu bez reālām saknēm"""
-    check50.run("python3 quadratic.py").stdin("1").stdin("2").stdin("5").stdout("Nav reālu sakņu").exit()
+    check50.run("python3 p10-1-kvadr.py").stdin("1").stdin("2").stdin("5").stdout("Nav reālu sakņu").exit()
 
 
 # Koeficients a ir 0
@@ -36,7 +36,7 @@ def no_roots():
 @check50.check(exists)
 def not_quadratic():
     """Pārbauda gadījumu, kad a ir 0"""
-    check50.run("python3 quadratic.py").stdin("0").stdin("2").stdin("3").stdout("Nav kvadrātvienādojums").exit()
+    check50.run("python3 p10-1-kvadr.py").stdin("0").stdin("2").stdin("3").stdout("Nav kvadrātvienādojums").exit()
 
 
 # Decimāli koeficienti
@@ -44,4 +44,4 @@ def not_quadratic():
 @check50.check(exists)
 def decimal_coefficients():
     """Pārbauda decimālus koeficientus"""
-    check50.run("python3 quadratic.py").stdin("0.5").stdin("-1.5").stdin("1").stdout("x1 = 2.0").stdout("x2 = 1.0").exit()
+    check50.run("python3 p10-1-kvadr.py").stdin("0.5").stdin("-1.5").stdin("1").stdout("x1 = 2.0").stdout("x2 = 1.0").exit()
